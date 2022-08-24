16da7a77d25d12cd99d4952fbe2930fc646a5050
Thomas Barreto <thomi.b137@gmail.com>
2022-08-24 09:37:33 -0300

    add footer

--- frontend/app/src/components/Footer.tsx
+++ frontend/app/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import {AiFillGithub, AiFillLinkedin} from 'react-icons/ai'
 
 export const Footer = () => {
   return (
-    <div className='self-end h-16 w-full bg-slate-300 flex items-center justify-center'>
+    <div className='h-12 bg-cyan-100 flex items-center justify-center'>
        <h5 className='font-bold text-center'>Creado por Thomas Barreto</h5> 
        <a target='__blank' className='mx-4' href='https://github.com/1thomas13'> 
         <AiFillGithub/>
